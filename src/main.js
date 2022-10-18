93d331589e5a0e03f4587e6a91fc1eda7a120294
Jhonattan Meira dos Santos <jhonattantecinf@gmail.com>
2022-10-17 21:56:16 -0300

    [Jhonattan Santos][17-10] Trocando Cores e Logo do Cartão através do DOM JavaScript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import "./css/index.css"
+
+var ccBgColorFirst = document.querySelector(
+  ".cc-bg svg > g g:nth-child(1) path"
+)
+var ccBgColorSecond = document.querySelector(
+  ".cc-bg svg > g g:nth-child(2) path"
+)
+
+var ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardColor(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "C69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColorFirst.setAttribute("fill", colors[type][0])
+  ccBgColorSecond.setAttribute("fill", colors[type][1])
+
+  ccLogo.setAttribute("src", "cc-" + type + ".svg")
+}
+
+setCardColor("visa")
